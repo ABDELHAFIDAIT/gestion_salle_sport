2ce4390714ff0e8024d1939b86fa4337199300aa
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-12-10 15:48:06 +0100

    Création de Footer

--- public/index.php
+++ public/index.php
@@ -3,17 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>OXYFIT</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="./assets/css/style.css">
+
+    <style>
+        .main{
+            background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('./assets/img/bg.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+    </style>
+
+
 </head>
 <body>
     <?php 
         require '../includes/header.php';
     ?>
 
-    <main>
-
+    <main class="main h-screen flex text-white items-center justify-center">
+        
     </main>
 
     <?php 
